1cd6f29f209a7bb39ee44d4a796a33e9d7e27faa
thom-droid <thom-droid@github.com>
2026-04-12 17:10:01 +0900

    refactor: POST /create
    - get batch ids and fetch all products to prevent n + 1

--- src/main/java/com/unexpected/arc2order/orders/application/OrderCommandService.java
+++ src/main/java/com/unexpected/arc2order/orders/application/OrderCommandService.java
@@ -2,6 +2,7 @@ package com.unexpected.arc2order.orders.application;
 
 import com.unexpected.arc2order.customer.application.CustomerQueryService;
 import com.unexpected.arc2order.customer.domain.Customer;
+import com.unexpected.arc2order.orders.api.request.CreateOrderItemRequest;
 import com.unexpected.arc2order.orders.api.request.CreateOrderRequest;
 import com.unexpected.arc2order.orders.api.response.CreateOrderResponse;
 import com.unexpected.arc2order.orders.api.response.OrderStatusUpdateResponse;
@@ -11,8 +12,11 @@ import com.unexpected.arc2order.orders.domain.OrderItemEntity;
 import com.unexpected.arc2order.orders.domain.OrderStatus;
 import com.unexpected.arc2order.orders.infrastructure.OrderItemRepository;
 import com.unexpected.arc2order.orders.infrastructure.OrderRepository;
+import com.unexpected.arc2order.product.application.ProductNotFoundException;
 import com.unexpected.arc2order.product.application.ProductQueryService;
 import com.unexpected.arc2order.product.domain.Product;
+import com.unexpected.arc2order.product.infrastructure.ProductRepository;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,6 +24,9 @@ import org.springframework.transaction.annotation.Transactional;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -29,19 +36,37 @@ public class OrderCommandService {
     private final OrderItemRepository orderItemRepository;
     private final CustomerQueryService customerQueryService;
     private final ProductQueryService productQueryService;
+    private final ProductRepository productRepository;
 
     @Transactional
     public CreateOrderResponse createOrder(CreateOrderRequest createOrderRequest) {
         Long customerId = createOrderRequest.customerId();
         Customer customer = customerQueryService.findById(customerId);
 
+        // batch ids
+        List<Long> productIds = createOrderRequest.items().stream()
+                .map(CreateOrderItemRequest::productId)
+                .toList();
+
+        // find all
+        Map<Long, Product> productMap = productRepository.findAllById(productIds).stream()
+                .collect(Collectors.toMap(Product::getId, Function.identity()));
+
+        // find not included in the found ids
+        if (productIds.size() != productMap.size()) {
+            List<Long> notFoundIds = productIds.stream().filter(id -> !productMap.containsKey(id))
+                    .toList();
+            throw new ProductNotFoundException(notFoundIds);
+        }
+
+        // map order item
         List<OrderItemEntity> orderItemEntities = createOrderRequest.items().stream()
-                .map(itemRequest -> {
+                .map(item -> {
                     OrderItemEntity oi = new OrderItemEntity();
-                    Product p = productQueryService.findById(itemRequest.productId());
+                    Product p = productMap.get(item.productId());
                     oi.setProduct(p);
                     oi.setUnitPrice(p.getPrice());
-                    oi.setQuantity(itemRequest.quantity());
+                    oi.setQuantity(item.quantity());
                     return oi;
                 })
                 .toList();
--- src/main/java/com/unexpected/arc2order/product/application/ProductNotFoundException.java
+++ src/main/java/com/unexpected/arc2order/product/application/ProductNotFoundException.java
@@ -1,7 +1,13 @@
 package com.unexpected.arc2order.product.application;
 
+import java.util.List;
+
 public class ProductNotFoundException extends RuntimeException {
     public ProductNotFoundException(Long productId) {
         super(String.format("Product with id %s not found", productId));
+    }
+
+    public ProductNotFoundException(List<Long> productIds) {
+        super(String.format("Product with id %s not found", productIds));
     }
 }
